388ed62618ab5ca9fe15f5a0006598a8ec06f662
dbansal3 <102465555+dbansal3@users.noreply.github.com>
2022-09-14 16:33:44 +0530

    Update nodejs.groovy

--- job_dsl/nodejs.groovy
+++ job_dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('dbansal3')
+            node / gitConfigEmail('deepanshubansal223@gmail.com')
         }
     }
     triggers {
